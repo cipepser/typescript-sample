3062ff73689a86fc3ac3aab91b32eecacbe12f43
cipepser <respepic@gmail.com>
2018-10-21 13:51:45 +0900

    add a line with unmatched type

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 let message = '';
 message = "TypeScript";
+message = 2;
 console.log("Hello!" + message);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
 let message: String = '';
 message = "TypeScript";
+message = 2;
 console.log("Hello!" + message);
