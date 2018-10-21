cf2ec7184a30e49c6a43a98ba29f7b22db1c1472
cipepser <respepic@gmail.com>
2018-10-21 13:50:22 +0900

    add src

---
+++ src/app.js
@@ -0,0 +1,4 @@
+let message = '';
+message = "TypeScript";
+console.log("Hello!" + message);
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,3 @@
+let message: String = '';
+message = "TypeScript";
+console.log("Hello!" + message);
